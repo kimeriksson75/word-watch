167b24e5b960d5777c6d1f40f25769e7351be25d
kimeriksson75 <kimeriksson75@gmail.com>
2024-01-15 20:04:23 +0100

    refactored

--- src/components/WordWatch.js
+++ src/components/WordWatch.js
@@ -1,16 +1,9 @@
 import { useState, useEffect, useMemo } from 'react';
 import './WordWatch.css';
-
+import * as Constants from '../constants';
 const WordWatch = () => {
 	const [time, setTime] = useState(new Date());
 	const [visibleChars, setVisibleChars] = useState([]);
-	
-	let chars = 'KLOCKANTÄRK FEMYISTIONI KVARTQIENZO TJUGOFEMPIM ÖVERKAMHALV ETTUSVLXTVÅ TREMYKYFYRA FEMSFLORSEX SJUÅTTAINIO TIOELVATOLV';
-	chars = chars.replaceAll(' ', '').split('');
-	const watch = [];
-	for (let i = 0; i < 11; i++) {
-			watch.push(chars.splice(0, 11));
-	}
 
 	let hours = time.getHours();
 	const minutes = time.getMinutes();
@@ -25,58 +18,54 @@ const WordWatch = () => {
 	const isCharVisible = (c) => memoizedVisibleChars.includes(c);
 
 	const visibleMinutes = {
-			5: [12, 13, 14],
-			10: [18, 19, 20],
-			15: [24, 25, 26, 27, 28],
-			20: [35, 36, 37, 38, 39, 40],
-			25: [35, 36, 37, 38, 39, 40, 41, 42, 43],
-			30: [55, 56, 57, 58],
-			35: [35, 36, 37, 38, 39, 40, 41, 42, 43],
-			40: [35, 36, 37, 38, 39, 40],
-			45: [24, 25, 26, 27, 28],
-			50: [18, 19, 20],
-			55: [12, 13, 14],
-			60: [],
-			0: [],
-	}
+		5: Constants.MINUTES_FIVE,
+		10: Constants.MINUTES_TEN,
+		15: Constants.MINUTES_QUARTER_PAST,
+		20: Constants.MINUTES_TWENTY,
+		25: Constants.MINUTES_TWENTY_FIVE,
+		30: Constants.MINUTES_HALF,
+		35: Constants.MINUTES_TWENTY_FIVE_TO,
+		40: Constants.MINUTES_TWENTY_TO,
+		45: Constants.MINUTES_QUARTER_TO,
+		50: Constants.MINUTES_TEN_TO,
+		55: Constants.MINUTES_FIVE_TO,
+		60: [],
+	};
 
 	const visibleHours = {
-			0: [115, 116, 117, 118],
-			1: [60, 61, 62],
-			2: [68, 69, 70],
-			3: [72, 73, 74],
-			4: [79, 80, 81, 82],
-			5: [84, 85, 86],
-			6: [92, 93, 94],
-			7: [96, 97, 98],
-			8: [99, 100, 101, 102],
-			9: [104, 105, 106],
-			10: [108, 109, 110],
-			11: [111, 112, 113, 114],
-			12: [115, 116, 117, 118],
-			13: [60, 61, 62],
-			14: [68, 69, 70],
-			15: [72, 73, 74],
-			16: [79, 80, 81, 82],
-			17: [84, 85, 86],
-			18: [92, 93, 94],
-			19: [96, 97, 98],
-			20: [99, 100, 101, 102],
-			21: [104, 105, 106],
-			22: [108, 109, 110],
-			23: [111, 112, 113, 114],
-			24: [115, 116, 117, 118],
-	}
-	const itIs = [0, 1, 2, 3, 4, 5, 6, 8, 9]
-	// const toOrPast =  (minutes > 57 && minutes < 3) ? [] : minutes > 30 ? [48, 49, 50, 51] : [45];
-	const disabledToPastMinutes = [28, 29, 30, 31, 32, 58, 59, 0, 1, 2];
-	const toOrPast = disabledToPastMinutes.includes(minutes) ? [] : minutes < 30 ? [48, 49, 50, 51] : [45];
+		0: Constants.HOURS_ZERO,
+		1: Constants.HOURS_ONE,
+		2: Constants.HOURS_TWO,
+		3: Constants.HOURS_THREE,
+		4: Constants.HOURS_FOUR,
+		5: Constants.HOURS_FIVE,
+		6: Constants.HOURS_SIX,
+		7: Constants.HOURS_SEVEN,
+		8: Constants.HOURS_EIGHT,
+		9: Constants.HOURS_NINE,
+		10: Constants.HOURS_TEN,
+		11: Constants.HOURS_ELEVEN,
+		12: Constants.HOURS_TWELVE,
+		13: Constants.HOURS_ONE,
+		14: Constants.HOURS_TWO,
+		15: Constants.HOURS_THREE,
+		16: Constants.HOURS_FOUR,
+		17: Constants.HOURS_FIVE,
+		18: Constants.HOURS_SIX,
+		19: Constants.HOURS_SEVEN,
+		20: Constants.HOURS_EIGHT,
+		21: Constants.HOURS_NINE,
+		22: Constants.HOURS_TEN,
+		23: Constants.HOURS_ELEVEN,
+		24: Constants.HOURS_TWELVE,
+	};
+	const toOrPast = Constants.DISABLED_TO_PAST_MINUTES.includes(minutes) ? [] : minutes < 30 ? Constants.PAST : Constants.TO;
 	useEffect(() => {
 		setVisibleChars([]);
-		setVisibleChars(visibleChars => [...visibleChars, ...itIs])
+		setVisibleChars(visibleChars => [...visibleChars, ...Constants.IT_IS])
 		setVisibleChars(visibleChars => [...visibleChars, ...toOrPast])
-		setVisibleChars(visibleChars => [...visibleChars, ...visibleMinutes[calcMinutes(minutes)]])
-		setVisibleChars(visibleChars => [...visibleChars, ...visibleHours[hours]])
+		setVisibleChars(visibleChars => [...visibleChars, ...visibleMinutes[calcMinutes(minutes)] || []])
+		setVisibleChars(visibleChars => [...visibleChars, ...visibleHours[hours] || []])
 	}, [hours, minutes]);
 
 	const memoizedVisibleChars = useMemo(() => {
@@ -93,11 +82,11 @@ const WordWatch = () => {
 
 	return (
 		<div className="word-watch-container" data-testid="word-watch">
-			{watch.map((row, i) => {
+			{Constants.WATCH.map((row, i) => {
 				return (
 					<div className="word-watch" key={i}>
 						{row.map((char, j) => {
-							const c = (i * 12) + j;
+							const c = (i * 11) + j;
 								return (
 									<div data-testid={c} className={`${isCharVisible(c) ? 'char char-visible' : 'char'}`} key={j}>{char}</div>
 									)
---
+++ src/constants.js
@@ -0,0 +1,43 @@
+export const WATCH = [
+    ['K', 'L', 'O', 'C', 'K', 'A', 'N', 'T', 'Ä', 'R', 'K'],
+    ['F', 'E', 'M', 'Y', 'I', 'S', 'T', 'I', 'O', 'N', 'I'],
+    ['K', 'V', 'A', 'R', 'T', 'Q', 'I', 'E', 'N', 'Z', 'O'],
+    ['T', 'J', 'U', 'G', 'O', 'F', 'E', 'M', 'P', 'I', 'M'],
+    ['Ö', 'V', 'E', 'R', 'K', 'A', 'M', 'H', 'A', 'L', 'V'],
+    ['E', 'T', 'T', 'U', 'S', 'V', 'L', 'X', 'T', 'V', 'Å'],
+    ['T', 'R', 'E', 'M', 'Y', 'K', 'Y', 'F', 'Y', 'R', 'A'],
+    ['F', 'E', 'M', 'S', 'F', 'L', 'Ö', 'R', 'S', 'E', 'X'],
+    ['S', 'J', 'U', 'Å', 'T', 'T', 'A', 'I', 'N', 'I', 'O'],
+    ['T', 'I', 'O', 'E', 'L', 'V', 'A', 'T', 'O', 'L', 'V'],
+];
+
+export const IT_IS = [0, 1, 2, 3, 4, 5, 6, 8, 9];
+export const TO = [42];
+export const PAST = [44, 45, 46, 47];
+export const DISABLED_TO_PAST_MINUTES = [28, 29, 30, 31, 32, 58, 59, 0, 1, 2];
+
+export const MINUTES_FIVE = [11, 12, 13];
+export const MINUTES_TEN = [17, 18, 19];
+export const MINUTES_QUARTER_PAST = [22, 23, 24, 25, 26];
+export const MINUTES_TWENTY = [33, 34, 35, 36, 37];
+export const MINUTES_TWENTY_FIVE = [33, 34, 35, 36, 37, 38, 39, 40];
+export const MINUTES_HALF = [51, 52, 53, 54];
+export const MINUTES_TWENTY_FIVE_TO = [33, 34, 35, 36, 37, 38, 39, 40];
+export const MINUTES_TWENTY_TO = [33, 34, 35, 36, 37];
+export const MINUTES_QUARTER_TO = [22, 23, 24, 25, 26];
+export const MINUTES_TEN_TO = [17, 18, 19];
+export const MINUTES_FIVE_TO = [11, 12, 13];
+
+export const HOURS_ZERO = [106, 107, 108, 109];
+export const HOURS_ONE = [55, 56, 57];
+export const HOURS_TWO = [63, 64, 65];
+export const HOURS_THREE = [66, 67, 68];
+export const HOURS_FOUR = [73, 74, 75, 76];
+export const HOURS_FIVE = [77, 78, 79];
+export const HOURS_SIX = [85, 86, 87];
+export const HOURS_SEVEN = [88, 89, 90];
+export const HOURS_EIGHT = [91, 92, 93, 94];
+export const HOURS_NINE = [96, 97, 98];
+export const HOURS_TEN = [99, 100, 101];
+export const HOURS_ELEVEN = [102, 103, 104, 105];
+export const HOURS_TWELVE = [106, 107, 108, 109];
